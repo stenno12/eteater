47fbcb8b6b7e4fa4ae716668c2f2d67c60136a11
stenno12 <stenno.sirdla@gmail.com>
2016-03-17 14:54:22 +0200

    test failid

---
+++ stest/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<?php include "ncoding.php"; ?>
+<head>
+	<title>Testleht</title>
+	<link rel="stylesheet" type="text/css" href="stiil.css">
+	<script src="ijscript.js"></script>
+</head>
+<body>
+
+	<div id="site_name">ELEKTRITEATER</div>
+	<ul class="nav">
+		<li><a class="active" href="index.php"><b>FILMID</b></a></li>
+		<li><a href="kinokava.html"><b>KINOKAVA</b></a></li>
+		<li><a href="info.html"><b>INFO</b></a></li>
+		<li><a href="test.html"><b>TEST</b></a></li>
+	</ul>
+	
+	<div class="content">
+	
+<?php
+try {
+	$conn = new PDO("sqlsrv:server = tcp:eteater.database.windows.net,1433; Database = eteaterdb", "stenno", "MjKpkslpn4s");
+	$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	
+	$stmt = $conn->prepare("SELECT * FROM TestFilm");
+
+	$stmt->execute();
+	$stmt->setFetchMode(PDO::FETCH_ASSOC); 
+	$result = $stmt->fetchAll();
+	
+	foreach ($result as $value){
+		echo "<div>";
+		echo '<img class="pic2" src="' . $value['PictureLocation'] . '">';
+		echo $value["Name"] . "<br><br>";
+		echo $value["DescriptionData"] . "<br><br>";
+		echo '<div class="divclear"></div>';
+		echo $value["DescriptionLong"];
+		echo "</div>";
+		echo "<hr>";
+	}
+
+}
+catch(PDOException $e){
+	echo "Connection failed:" . $e->getMessage() . "<br>";
+}
+?>
+	
+
+
+	</div>
+</body>
