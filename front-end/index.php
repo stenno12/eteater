9745e120205fe5a748afb575aa205563202dd4cd
mrtntdr <merntdr@gmail.com>
2016-04-07 14:34:45 +0300

    asdf

--- front-end/index.php
+++ front-end/index.php
@@ -1,8 +1,5 @@
+<?php include "ncoding.php"; ?>
 <?php include_once("languages/lang.php"); ?>
-
-
-
-
 <!doctype html>
 <html lang="et">
     <head>
@@ -11,113 +8,92 @@
         <meta name="description" content="eTeater on arthouse kino Tartus" />
         <meta name="keywords" content="kino, tartu, arthouse, filmid, tartus, elektriteater" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="https://fonts.googleapis.com/css?family=Roboto:300|Roboto+Slab:300" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300|Roboto+Slab:300" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" type="text/css" href="style.css" />
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    	<![endif]-->
     </head>
     <body>
         <header>
-            <menu>
-                <div id="logo"><a href="#pealeht"><img src="logo.png" /></a></div>
-                <ul>
-                    <li><a href="#kava" class="active">Kava</a></li>
-                    <li><a href="#sarjad"> <?php echo $lang['menu_series'] ?> </a></li>
-                    <li><a href="#kinost">Kinost</a></li>
-                    <li><a href="#arhiiv">Arhiiv</a></li>
-                    <li><a href="#kontakt">Kontakt</a></li>
-                </ul>
-                <ul id="social_btn">
-                    <li><a href="https://www.facebook.com/elektriteater"><img src="fb.png" alt="fb" /></a></li>
-                    <li><a href="https://www.instagram.com/elektriteater/"><img src="ig.png" alt="ig" /></a></li>
-					<li><a href="index.php?lang=et"></a></li>
-					<li><a href="index.php?lang=en"></a></li>
-                </ul>
+			<menu>
+				<span class="keelevalik"><a href="index.php?lang=et">ET</a></span>
+				<span class="keelevalik"><a href="index.php?lang=en">EN</a></span>
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a href="#pealeht" class="logo"><img class="logoPilt" src="logo.png" /></a>
+            	</div>
+                <div id="navbar" class="navbar-collapse collapse">
+					<ul class="navigeering nav navbar-nav">
+						<li><a href="#kava" class="active"><?php echo $lang['menu_schedule'] ?></a></li>
+						<li><a href="#sarjad"><?php echo $lang['menu_series'] ?></a></li>
+						<li><a href="#kinost"><?php echo $lang['menu_about'] ?></a></li>
+						<li><a href="#arhiiv"><?php echo $lang['menu_archive'] ?></a></li>
+						<li><a href="#kontakt"><?php echo $lang['menu_contact'] ?></a></li>
+						<li class="social_btn"><a href="https://www.facebook.com/elektriteater"><img src="fb.png" alt="fb" /></a></li>
+						<li class="social_btn"><a href="https://www.instagram.com/elektriteater/"><img src="ig.png" alt="ig" /></a></li>
+					</ul>
+                </div>
+
             </menu>
         </header>
-        <div class="rida">
-            <div id="sisukava" class="col col9">
-                <h1>Kinokava</h1>
+        <div class="row">
+            <div id="sisukava" class="col-xs-12 col-sm-10 col-md-9">
+                <h1>Varsti kinos</h1>
 
                 <!-- Siin on üksiku filmi disain info -->
-                <div style="film">
-                    <div id="kp2ev" class="col col2">
-                        <h4 id="kuup2evNr">15</h4>
-                        <h4 id="kuup2ev">Apr</h4>
-                        <h4 id="kuup2ev">19:00</h4>
-                    </div>
-                    <div id="fotokonteiner" class="col col5">
-                        <img src="filmipostrid/laenatud.jpg" class="filmifoto" />
-                        <figcaption>
-                            <h3 class="pildipealne">“Laenatud naene” elava muusikaga</h3>
-                        </figcaption>
-                    </div>
-                    <div id="filmikirjeldus" class="col col5">
-                        Elektriteatri viienda sünnipäeva puhul näitame märtsis kõigi seansside ette eeskava asemel Eesti kõige vanemat komöödiat pealkirjaga “Laenatud naene”. Tummfilmi saadab klaveril pianist Jorma Toots. Nii saab ka tänane publik osa vanade kinode hõngust, kus film ja elav muusika käisid käsikäes. “Laenatud naene” on tõenäoliselt 1913. aastal Tallinnas filmitud 12-minutiline komöödia
-                    </div>
-                    <div id="filmiinfo" class="col col10">
-                        <div class="col col3"><b>Riik</b><br /> Eesti</div>
-                        <div class="col col3"><b>Režissöör</b><br /> Kenneth Branagh</div>
-                        <div class="col col3"><b>Pikkus</b><br /> 242 min</div>
-                        <div class="col col3">English friendly</div>
-                    </div>
-                </div>
-                <!-- Siin on üksiku filmi disain info -->
-                <div style="film">
-                    <div id="kp2ev" class="col col2">
-                        <h4 id="kuup2evNr">15</h4>
-                        <h4 id="kuup2ev">Apr</h4>
-                        <h4 id="kuup2ev">19:00</h4>
-                    </div>
-                    <div id="fotokonteiner" class="col col5">
-                        <img src="filmipostrid/laenatud.jpg" class="filmifoto" />
-                        <figcaption>
-                            <span></span>
-                            <h3 class="pildipealne">“Laenatud naene” elava muusikaga</h3>
-                        </figcaption>
-                    </div>
-                    <div id="filmikirjeldus" class="col col5">
-                        Elektriteatri viienda sünnipäeva puhul näitame märtsis kõigi seansside ette eeskava asemel Eesti kõige vanemat komöödiat pealkirjaga “Laenatud naene”. Tummfilmi saadab klaveril pianist Jorma Toots. Nii saab ka tänane publik osa vanade kinode hõngust, kus film ja elav muusika käisid käsikäes. “Laenatud naene” on tõenäoliselt 1913. aastal Tallinnas filmitud 12-minutiline komöödia
-                    </div>
-                    <div id="filmiinfo" class="rida col10">
-                        <div class="col col3"><b>Riik</b><br /> Eesti</div>
-                        <div class="col col3"><b>Režissöör</b><br /> Kenneth Branagh</div>
-                        <div class="col col3"><b>Pikkus</b><br /> 242 min</div>
-                        <div class="col col3">English friendly</div>
-                    </div>
-                </div>
-                <!-- Siin on üksiku filmi disain info -->
-                <div style="film">
-                    <div id="kp2ev" class="col col2">
-                        <h4 id="kuup2evNr">15</h4>
-                        <h4 id="kuup2ev">Apr</h4>
-                        <h4 id="kuup2ev">19:00</h4>
-                    </div>
-                    <div id="fotokonteiner" class="col col5">
-                        <img src="filmipostrid/laenatud.jpg" class="filmifoto" />
-                        <figcaption>
-                            <span></span>
-                            <h3 class="pildipealne">“Laenatud naene” elava muusikaga</h3>
-                        </figcaption>
-                        <span></span>
-                    </div>
-                    <div id="filmikirjeldus" class="col col5">
-                        Elektriteatri viienda sünnipäeva puhul näitame märtsis kõigi seansside ette eeskava asemel Eesti kõige vanemat komöödiat pealkirjaga “Laenatud naene”. Tummfilmi saadab klaveril pianist Jorma Toots. Nii saab ka tänane publik osa vanade kinode hõngust, kus film ja elav muusika käisid käsikäes. “Laenatud naene” on tõenäoliselt 1913. aastal Tallinnas filmitud 12-minutiline komöödia
-                    </div>
-                    <div id="filmiinfo" class="rida col10">
-                        <div class="col col3"><b>Riik</b><br /> Eesti</div>
-                        <div class="col col3"><b>Režissöör</b><br /> Kenneth Branagh</div>
-                        <div class="col col3"><b>Pikkus</b><br /> 242 min</div>
-                        <div class="col col3">English friendly</div>
-                    </div>
-                </div>
+				<?php
+				try {
+					$conn = new PDO("sqlsrv:server = tcp:eteater.database.windows.net,1433; Database = eteaterdb", "stenno", "MjKpkslpn4s");
+					$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+					$stmt = $conn->prepare("SELECT * FROM TestFilm");
+					$stmt->execute();
+					$stmt->setFetchMode(PDO::FETCH_ASSOC);
+					$result = $stmt->fetchAll();
+
+					foreach ($result as $value){
+						echo '<div class="film row">';
+							echo '<div class="aeg film col-xs-2 col-sm-2 col-md-2">';
+								echo '<h4 class="n2dalaP2ev">E</h4>';
+								echo '<h4 class="kuup2ev">21.03<br />19:00</h4>';
+							echo '</div>';
+							echo '<div class="fotokonteiner col col-xs-10 col-sm-5 col-md-5">';
+								echo '<img class="filmifoto"  src="'. $value['PictureLocation'] .'" />';
+								echo '<figcaption><h3 class="pildipealne">'. $value["Name"] .'</h3></figcaption>';
+							echo '</div>';
+							echo '<div class="filmikirjeldus col-xs-12 col-sm-5 col-md-5">'.$value["DescriptionLong"].'</div>';
+							echo '<div class="filmiinfoblokk col-xs-12 col-sm-10 col-md-10">';
+								echo '<div class="filmiinfo col-xs-6 col-sm-3 col-md-3"><b>Riik</b><br />' . $value["DescriptionCountry"] .'</div>';
+								echo '<div class="filmiinfo col-xs-6 col-sm-3 col-md-3"><b>Režissöör</b><br />' . $value["DescriptionFilmDirector"] .'</div>';
+								echo '<div class="filmiinfo col-xs-6 col-sm-3 col-md-3"><b>Pikkus</b><br />' . $value["DescriptionRunningTime"] .'</div>';
+								echo '<div class="filmiinfo col-xs-6 col-sm-3 col-md-3">' . $value["DescriptionAdditionalInfo"] .'</div>';
+							echo '</div>';
+						echo '</div>';
+					}
+
+					catch(PDOException $e){
+						echo "Connection failed:" . $e->getMessage() . "<br>";
+					}
+					?>
             </div>
 
-            <div id="parembox" class="col col3">
+            <div id="parembox" class="col-xs-12 col-sm-2 col-md-3">
                 <h2>Seansid</h2>
-                <input type="date"   value="dd-mm-yyyy" onkeypress="return false">
+                <p>See menüü siin on jõhkralt pooleli, kunagi järgmine kord täieneb</p>
             </div>
         </div>
         <footer>
-
         </footer>
     </body>
 </html>
